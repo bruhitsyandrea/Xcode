982c1c9f00ab25a2d0d0e54870436977791a3bfd
bruhitsyandrea <lisachen20000226@gmail.com>
2023-09-13 19:55:16 -0700

    add HelloWorld

---
+++ HelloWorld/HelloWorld/HelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloWorldApp.swift
+//  HelloWorld
+//
+//  Created by Andrea Chen on 2023/9/13.
+//
+
+import SwiftUI
+
+@main
+struct HelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
