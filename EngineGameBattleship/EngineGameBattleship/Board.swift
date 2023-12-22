1a27d58c792293af0d4c4ed421c413976eaf6adf
bruhitsyandrea <lisachen20000226@gmail.com>
2023-12-22 23:11:33 +0800

    for macos

---
+++ EngineGameBattleship/EngineGameBattleship/Board.swift
@@ -0,0 +1,13 @@
+//
+//  Board.swift
+//  EngineGameBattleship
+//
+//  Created by Andrea Chen on 2023/12/22.
+//
+
+import Foundation
+
+class Board {
+    //private let rows, cols: Int
+    //private var board: [[CellType]]
+}
